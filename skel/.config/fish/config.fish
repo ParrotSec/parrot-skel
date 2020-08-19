d416bf54bff957fcce80cb5ca550fd45737fe6d5
dmknght <dmknght@localhost.localdomain>
2020-08-19 17:24:44 +0700

    Add fish support (user)

---
+++ skel/.config/fish/config.fish
@@ -0,0 +1,15 @@
+function fish_greeting
+    echo "Welcome to Parrot OS"
+end
+
+function fish_prompt
+    echo (set_color red)"┌──["(set_color normal)"$USER@"(set_color cyan)"$hostname"(set_color red)"]─["(set_color yellow)(date "+%H:%M-%d/%m")(set_color red)"]─["(set_color blue)"$PWD"(set_color red)"]"
+    echo (set_color red)"└──╼"(set_color yellow) "\$"(set_color normal)
+end
+
+alias ls='ls -lh --color=auto'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
