acfc3875d1166cb9c7d22b3c36978e0b60bb2ebf
dmknght <dmknght@localhost.localdomain>
2020-08-20 03:07:28 +0700

    Fix $PATH

--- skel/.config/fish/config.fish
+++ skel/.config/fish/config.fish
@@ -7,9 +7,14 @@ function fish_prompt
     echo (set_color red)"└╼"(set_color green)"$USER"(set_color yellow)(set_color yellow)"\$"(set_color normal)
 end
 
+set PATH ~/.local/bin /snap/bin /usr/sandbox/ /usr/local/bin /usr/bin /bin /usr/local/games /usr/games /usr/share/games /usr/local/sbin /usr/sbin /sbin $PATH
+
 alias ls='ls -lh --color=auto'
 alias dir='dir --color=auto'
 alias vdir='vdir --color=auto'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
+set DEBEMAIL "dmknght@parrotsec.org"
+set DEBFULLNAME "Nong Hoang Tu"
+export DEBEMAIL DEBFULLNAME
